87d9ba64ca92db2dc6798d30b281dfb6e014ebc5
Paulo Mendes <paulo.mendes87@gmail.com>
2015-05-07 12:05:55 -0300

    Example App

--- Example/EventHorizon/EVHViewController.m
+++ Example/EventHorizon/EVHViewController.m
@@ -7,23 +7,54 @@
 //
 
 #import "EVHViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "EventHorizon.h"
 
-@interface EVHViewController ()
+#define METERS_PER_MILE 1609.344
+
+@interface EVHViewController () <CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UITextField *latitudeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *longitudeTextField;
 
 @end
 
 @implementation EVHViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // This is the Catch!!!
+    [[EventHorizon instance] enable];
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    [self.locationManager startUpdatingLocation];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Actions
+
+- (IBAction)fakeItButtonAction:(id)sender {
+    NSInteger lat = [self.latitudeTextField.text integerValue];
+    NSInteger lng = [self.longitudeTextField.text integerValue];
+    
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:lat longitude:lng];
+    [[EventHorizon instance] fakeLocation:location];
+}
+
+#pragma mark - CLLocationManagerDelegate Implementation
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *location = [locations firstObject];
+    
+    NSLog(@"lat: %@  lng: %@", @(location.coordinate.latitude), @(location.coordinate.longitude));
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    [self.map setRegion:viewRegion animated:YES];
 }
 
 @end
